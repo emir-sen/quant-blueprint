322f4fa9958e90878b1dd4f0aac9125845d1772d
Emir Şen <senemirsamet@gmail.com>
2026-08-03 11:22:02 +0300

    Complete lesson 008: introduction to lists

---
+++ lesson_008.py
@@ -0,0 +1,10 @@
+meyveler = ["Muz", "Armut", "Elma"]
+
+print(meyveler[1])
+
+meyveler.append("Çilek")
+
+print(len(meyveler))
+print(meyveler[len(meyveler) - 1])
+print(meyveler[-1])
+print(meyveler[-2])
