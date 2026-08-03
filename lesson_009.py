73f9aed6804086ae1926da33ca30cd1471667950
Emir Şen <senemirsamet@gmail.com>
2026-08-03 11:34:41 +0300

    Complete lesson 009: introduction to for loops

---
+++ lesson_009.py
@@ -0,0 +1,16 @@
+meyveler = ["Muz", "Çilek", "Armut", "Elma"]
+
+for meyve in meyveler:
+    print(meyve)
+
+sayilar = [10, 20, 30]
+
+for sayi in sayilar:
+    print(5 + sayi)
+
+toplam = 0
+
+for sayi in sayilar:
+    toplam = toplam + sayi
+
+print(toplam)
