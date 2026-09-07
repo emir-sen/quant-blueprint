198b64b18ac29faaa54fdd59ad380e947c7233ca
Emir Şen <senemirsamet@gmail.com>
2026-09-08 02:08:38 +0300

    feat: list slicing and indexing in lesson 16

---
+++ lesson_016.py
@@ -0,0 +1,10 @@
+bist_fiyatlar = [300, 305, 302, 310, 315, 312, 320]
+
+# 1. En son günün fiyatı
+print(f"Son gün fiyatı: {bist_fiyatlar[-1]}")
+
+# 2. İlk 3 günün fiyatları
+print(f"İlk üç gün fiyatı: {bist_fiyatlar[:3]}")
+
+# 3. Son 2 günün fiyatları
+print(f"Son iki gün fiyatı: {bist_fiyatlar[-2:]}")
